ff92a5cdff6a75546e0b9010b9eb6672095a708c
Foreverhighness <Foreverhighness@gmail.com>
2017-02-02 12:07:36 +0800

    initial commit

---
+++ GUI/GUI/MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int outputFileNumber = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FileRadioButton.Click += FileRadioButton_Click;
+            DirectoryRadioButton.Click += DirectoryRadioButton_Click;
+            MyButton.Click += MyButton_Click;
+        }
+
+        private void DirectoryRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileTextBox.Text = "";
+
+            FileRadioButton.IsChecked = false;
+            FileTextBox.IsEnabled = false;
+            FilesBrowser.IsEnabled = false;
+            DirectoryBrowser.IsEnabled = true;
+            DirectoryRadioButton.IsChecked = true;
+            DirectoryTextBox.IsEnabled = true;
+        }
+
+        private void FileRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            DirectoryTextBox.Text = "";
+
+            FileRadioButton.IsChecked = true;
+            FileTextBox.IsEnabled = true;
+            FilesBrowser.IsEnabled = true;
+            DirectoryBrowser.IsEnabled = false;
+            DirectoryRadioButton.IsChecked = false;
+            DirectoryTextBox.IsEnabled = false;
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FileTextBox.Text != "")
+            {
+                Solve(FileTextBox.Text, TextBox.Text);
+                MessageBox.Show("OK");
+            }
+            else if (DirectoryTextBox.Text != "")
+            {
+                DirectoryInfo myDirectoryInfo = new DirectoryInfo(DirectoryTextBox.Text);
+                foreach (FileInfo file in myDirectoryInfo.GetFiles("*.2dx"))
+                    Solve(file.FullName, TextBox.Text);
+                MessageBox.Show("OK");
+            }
+            if (checkBox.IsChecked == true)
+                Close();
+        }
+
+        private void FilesBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog1.Filter = "2dx files (*.2dx)|*.2dx|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == true)
+                FileTextBox.Text = openFileDialog1.FileName;
+        }
+
+        private void DirectoryBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog myFolderBrowserDialog = 
+                new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = myFolderBrowserDialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.Cancel)
+                return;
+
+            DirectoryTextBox.Text = myFolderBrowserDialog.SelectedPath.Trim();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog myFolderBrowserDialog =
+                new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = myFolderBrowserDialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.Cancel)
+                return;
+
+            TextBox.Text = myFolderBrowserDialog.SelectedPath.Trim();
+        }
+
+        public void Solve(string inputFilePath, string outputPath)
+        {
+            string riff = "RIFF";
+            int musicNumber = 1;
+            byte[] myByte = new byte[4];
+            FileStream inputFileStream = null;
+            try
+            {
+                inputFileStream = new FileStream(inputFilePath, FileMode.Open);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            try
+            {
+                while (inputFileStream.Read(myByte, 0, 4) != 0)
+                {
+                    if (Encoding.Default.GetString(myByte) == riff)
+                    {
+                        inputFileStream.Read(myByte, 0, 4);
+                        int wavFileLength = BitConverter.ToInt32(myByte, 0);
+                        //MessageBox.Show(".wav file size: " + wavFileLength.ToString());
+
+
+                        // 文件输出位置调整
+                        string outputFileName = outputPath;
+                        if (outputFileName == "")
+                            outputFileName = string.Format("{0}_{1}.wav", outputFileNumber, musicNumber++);
+                        else
+                            outputFileName += string.Format("\\{0}_{1}.wav", outputFileNumber, musicNumber++);
+
+                        FileStream outputFileStream = null;
+                        try
+                        {
+                            outputFileStream = new FileStream(outputFileName, FileMode.Create);
+                        }
+                        catch (Exception e)
+                        {
+                            MessageBox.Show(e.Message);
+                        }
+
+
+                        outputFileStream.Write(Encoding.Default.GetBytes(riff), 0, 4);
+                        outputFileStream.Write(myByte, 0, 4);
+
+                        byte[] temp = new byte[wavFileLength];
+                        inputFileStream.Read(temp, 0, wavFileLength);
+                        outputFileStream.Write(temp, 0, wavFileLength);
+
+                        outputFileStream.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                inputFileStream.Close();
+            }
+            outputFileNumber++;
+        }
+
+    }
+}
